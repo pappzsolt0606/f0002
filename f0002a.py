cc2cd1b4124461fc0b736001cc3a357726b5ad5c
pappzsolt0606 <71691840+pappzsolt0606@users.noreply.github.com>
2020-10-16 08:40:28 +0200

    Add files via upload

---
+++ f0002a.py
@@ -0,0 +1,11 @@
+#F0002a: Melyik állat nevét nem írja ki a program? (Csak a válasz megadását követően ér lefuttatni a programot:))
+    
+cica = 'kutya'
+kutya = 'egér'
+egér = 'cica'
+print(cica)
+print('kutya')
+print(egér)
+print('cica')
+print(kutya)
+print('egér')
